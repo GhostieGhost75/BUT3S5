16739f2c08952be05c1a2f115f835e268fd543ce
Lucas Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2025-09-22 16:24:45 +0200

    Synchronized avec Exclusion (exo 1)

--- R505ProgAvancee/TP2/Affichage.java
+++ R505ProgAvancee/TP2/Affichage.java
@@ -7,17 +7,23 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
 
+class Exclusion{};
 public class Affichage extends Thread{
-	String texte; 
+	String texte;
+    static final Exclusion exclusionImpression = new Exclusion();
 
 	public Affichage (String txt){texte=txt;}
 	
 	public void run(){
 
-		for (int i=0; i<texte.length(); i++){
-		    System.out.print(texte.charAt(i));
-		    try {sleep(100);} catch(InterruptedException e){};
-		}
+        // *
+        synchronized (exclusionImpression) { //verrou avec exclusion, possible de verouiller System.out à la place
+            for (int i=0; i<texte.length(); i++){
+                System.out.print(texte.charAt(i)); //ressource critique
+                try {sleep(100);} catch(InterruptedException e){};
+            }
+        }
+        // section critique
 
 	}
 }
