36f0671fd556d980c34c585719e7042f3365d339
Lucas Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2025-09-22 15:15:03 +0200

    deux mobiles aller retour

--- R505ProgAvancee/TP1/UneFenetre.java
+++ R505ProgAvancee/TP1/UneFenetre.java
@@ -1,18 +1,24 @@
 import java.awt.*;
 import javax.swing.*;
 
-class UneFenetre extends JFrame 
+class UneFenetre extends JFrame
 {
-    UnMobile sonMobile;
+    UnMobile sonMobile1;
+    UnMobile sonMobile2;
     private final int LARG=400, HAUT=250;
-    
+
     public UneFenetre() {
         setSize(LARG, HAUT);
         Container leConteneur = getContentPane();
-        sonMobile = new UnMobile(LARG, HAUT); //enfant de la fenêtre
-        leConteneur.add(sonMobile);
-        Thread laThread = new Thread(sonMobile); //runnable dans thread
+        leConteneur.setLayout(new GridLayout(2, 1));
+        sonMobile1 = new UnMobile(LARG, HAUT/2); //enfant de la fenêtre
+        leConteneur.add(sonMobile1);
+        sonMobile2 = new UnMobile(LARG, HAUT/2); //enfant de la fenêtre
+        leConteneur.add(sonMobile2);
+        Thread laThread1 = new Thread(sonMobile1); //runnable dans thread
+        Thread laThread2 = new Thread(sonMobile2); //runnable dans thread
         setVisible(true);
-        laThread.start();
+        laThread1.start();
+        laThread2.start();
     }
 }
