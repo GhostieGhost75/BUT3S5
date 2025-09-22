bff42343faa4c73302e22965a308c30581ad7df7
Lucas Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2025-09-22 16:29:12 +0200

    Test avec plus de tâches

--- R505ProgAvancee/TP2/Main.java
+++ R505ProgAvancee/TP2/Main.java
@@ -8,12 +8,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		Affichage TA = new Affichage("AAA");
 		Affichage TB = new Affichage("BB");
+        Affichage TC = new Affichage("C");
+        Affichage TD = new Affichage("DDDD");
+        Affichage TE =  new Affichage("EEEEE");
+        Affichage TF = new Affichage("FFFFFF");
+        Affichage TG = new Affichage("GGGGGGG");
+        Affichage TH = new Affichage("HHHHHHHH");
 
-		TB.start();
-
-		TA.start();
+        TA.start();
+        TB.start();
+        TC.start();
+        TD.start();
+        TE.start();
+        TF.start();
+        TG.start();
+        TH.start();
 	}
 
 }
