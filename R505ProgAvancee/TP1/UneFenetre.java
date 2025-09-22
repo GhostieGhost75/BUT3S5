e826072f913e34a31fa31d2d98b730fec791e47e
Lucas Ferreira de Sousa <lucassousa2005.ls@gmail.com>
2025-09-22 15:10:00 +0200

    un mobile avance

--- R505ProgAvancee/TP1/UneFenetre.java
+++ R505ProgAvancee/TP1/UneFenetre.java
@@ -6,12 +6,13 @@ class UneFenetre extends JFrame
     UnMobile sonMobile;
     private final int LARG=400, HAUT=250;
     
-    public UneFenetre()
-    {
-	// TODO 
-	// ajouter sonMobile a la fenetre
-	// creer une thread laThread avec sonMobile
-	// afficher la fenetre
-	// lancer laThread 
+    public UneFenetre() {
+        setSize(LARG, HAUT);
+        Container leConteneur = getContentPane();
+        sonMobile = new UnMobile(LARG, HAUT); //enfant de la fenêtre
+        leConteneur.add(sonMobile);
+        Thread laThread = new Thread(sonMobile); //runnable dans thread
+        setVisible(true);
+        laThread.start();
     }
 }
